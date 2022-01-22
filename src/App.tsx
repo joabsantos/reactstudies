bed863fd5fd3b2fa888c18e668013eec66a2e258
joab <joabzatch@hotmail.com>
2022-01-22 08:00:25 -0300

    criando tela newroom, estilização das telas e autenticação e estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
+import { createContext, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { firebase, auth } from './services/firebase';
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+type User = {
+  id: string,
+  name: string,
+  avatar: string
+}
+
+type AuthContextType = {
+  user: User | undefined,
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
+
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(provider);
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL)
+        throw new Error("Missing information from Google Account");
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+
+  }
+
   return (
-     <Home />
-  ); 
+    <BrowserRouter>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/rooms/new" element={<NewRoom />}></Route>
+        </Routes>
+      </AuthContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
